8c3d0d69afca12a006cb1fc090a7b5ccc8dd845d
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2026-06-17 11:45:40 +0100

    Exercise 2: Live-editable toast preview - solution

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -12,6 +13,15 @@ function ToastPlayground() {
   const [messageVariant, setMessageVariant] = React.useState(
     VARIANT_OPTIONS[0]
   );
+  const [showToast, setShowToast] = React.useState(false);
+
+  const handlePop = () => {
+    setShowToast(true);
+  };
+
+  const handleDismiss = () => {
+    setShowToast(false);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -19,6 +29,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {showToast && (
+        <Toast messageVariant={messageVariant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -68,7 +84,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={handlePop}>Pop Toast!</Button>
           </div>
         </div>
       </div>
